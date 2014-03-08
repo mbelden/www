3606afe55db674deed3028ca3cd4b43323b88dfd
mbelden <beldenm@gmail.com>
2014-03-07 21:16:41 -0600

    update routes

--- routes.py
+++ routes.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/resume')
+def resume():
+  return render_template('resume.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
  
 if __name__ == '__main__':
   app.run(debug=True)
